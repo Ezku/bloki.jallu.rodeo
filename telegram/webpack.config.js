0895a932148fe3fc5fb3e3ad095d1f97c328b165
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-09-07 17:14:23 +0300

    replace iconv-loader with node-noop instead of ignoring outright
    https://github.com/kadirahq/lokka-transport-http/issues/22#issuecomment-313366887

--- telegram/webpack.config.js
+++ telegram/webpack.config.js
@@ -12,11 +12,11 @@ const path = require('path');
 
 const target = require('./webpack/target');
 const dotenvLoader = require('./webpack/dotenv');
-const ignorePlugin = require('./webpack/ignorePlugin');
+const replaceModulePlugin = require('./webpack/replaceModulePlugin');
 
 module.exports = createConfig([
   target('node'),
-  ignorePlugin(/iconv-loader/),
+  replaceModulePlugin(/\/iconv-loader$/, 'node-noop'),
   entryPoint('./lib/js/src/index.js'),
   setOutput('./build/app.js'),
   defineConstants({
---
+++ telegram/webpack/replaceModulePlugin.js
@@ -0,0 +1,5 @@
+const { NormalModuleReplacementPlugin } = require('webpack');
+
+module.exports = function replaceModulePlugin(pattern, moduleName) {
+  return (context, { addPlugin }) => addPlugin(new NormalModuleReplacementPlugin(pattern, moduleName));
+};
