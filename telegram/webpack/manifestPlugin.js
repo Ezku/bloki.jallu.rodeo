d4caa633a0e1b9e5263d499d95d6b40cc009471e
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-09-09 16:02:51 +0300

    add manifestPlugin for creating a standalone webpack boostrap file

---
+++ telegram/webpack/manifestPlugin.js
@@ -0,0 +1,11 @@
+const { CommonsChunkPlugin } = require('webpack').optimize;
+
+module.exports = function manifestPlugin(manifestName = "manifest") {
+  return (context, { addPlugin }) => addPlugin(
+    new CommonsChunkPlugin({
+      filename: `${manifestName}.js`,
+      name: manifestName,
+      minChunks: Infinity
+    })
+  );
+};
