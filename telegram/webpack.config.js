3fed6030ad996fb8027a7209b2931b5c2105d50f
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-09-09 16:03:14 +0300

    attempt to split app files from node_modules for development

--- telegram/webpack.config.js
+++ telegram/webpack.config.js
@@ -21,21 +21,26 @@ const target = require('./webpack/target');
 const dotenvLoader = require('./webpack/dotenv');
 const replaceModulePlugin = require('./webpack/replaceModulePlugin');
 const bucklescriptLoader = require('./webpack/bucklescriptLoader');
+const vendorChunkPlugin = require('./webpack/vendorChunkPlugin');
+const manifestPlugin = require('./webpack/manifestPlugin');
 
 module.exports = createConfig([
   target('node'),
   babel(),
   replaceModulePlugin(/\/iconv-loader$/, 'node-noop'),
   entryPoint('./src/index.re'),
-  setOutput('./build/app.js'),
-  match("*.re", { exclude: path.resolve('node_modules') }, [
+  setOutput('./build/app.[name].js'),
+  match("*.re", { exclude: path.resolve('node_modules'), include: path.resolve('src') }, [
     bucklescriptLoader()
   ]),
   defineConstants({
     'process.env.NODE_ENV': process.env.NODE_ENV
   }),
   dotenvLoader(),
-  env('development', []),
+  env('development', [
+    vendorChunkPlugin("node_modules"),
+    manifestPlugin("app")
+  ]),
   env('production', [
     uglify({
       uglifyOptions: {
